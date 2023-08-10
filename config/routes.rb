12e14e6b81263351bde4baf04060e9c3a5221ba9
Gary Topping <toppinggary@hotmail.com>
2023-08-10 15:16:40 +0900

    finished bookmarks

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[new create]
+  end
+  resources :bookmarks, only: %i[destroy]
 end
+
+# A user can see all the lists
+# GET "lists"
+# A user can see the details of a given list and its name
+# GET "lists/42"
+# A user can create a new list
+# GET "lists/new"
+# POST "lists"
