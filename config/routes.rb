c50cff32e07c5e98b2c6c866ce0225bb1dc80afb
Gary Topping <toppinggary@hotmail.com>
2023-08-11 10:09:39 +0900

    changing list to root path in controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: 'lists#index'
+  root_to: 'lists#index'
   resources :lists, only: %i[index show new create destroy] # do
    # resources :bookmarks, only: %i[new create]
   # end
